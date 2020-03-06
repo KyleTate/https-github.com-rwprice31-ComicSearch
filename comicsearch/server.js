fe6404f40f05181d48ac3f46462638e1036a1e53
Jose <jcantillo@ggc.edu>
2020-03-06 14:25:48 -0500

    ignore idea into gitignore

--- comicsearch/server.js
+++ comicsearch/server.js
@@ -1,4 +1,6 @@
 var http = require('http');
+const express = require('express');
+const app = express();
 
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -6,4 +8,8 @@ function onRequest(request, response) {
     response.end();
 }
 http.createServer(onRequest).listen(8000);
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(8000, () => console.log('Example app listening on port 3000!'));
 
