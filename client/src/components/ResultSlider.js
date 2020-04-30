6c4d72b305862c36e0d47b41e749638d52104e65
Jose <jcantillo@ggc.edu>
2020-04-29 21:13:05 -0400

    create Map data structure to hold array values of issue id's for a respective series. This is on the ResultSlider on the Results page for a characteer. This push has an error of not being able to iterate throughout all the key value pairs of the map however. also, the resultslider will rerender from clicking an image from the image0grid for some reason

--- client/src/components/ResultSlider.js
+++ client/src/components/ResultSlider.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Slider from "react-slick";
 import ser1 from './series1.jpg';
 import ser2 from './series2.jpg';
@@ -7,20 +7,25 @@ import ser4 from './series4.jpg';
 import ser5 from './series5.jpg';
 import {Link} from "react-router-dom";
 
+// let myMap = new Map();
+
 export default class ResultSlider extends Component {
 
+    myMap = new Map();
+    temp = [];
+    charName;
+    image;
+    issue_cover_id;
+    series;
+    // renderedOnce = true;
 
-
-charName;
-image;
-issue_cover_id;
-series;
 
     constructor(props) {
         super(props);
         this.state = {
             list: this.props.List,
             Character: this.props.Character,
+            renderedOnce: false,
             items: [
                 {
                     id: 0,
@@ -50,6 +55,21 @@ series;
 
             ]
         }
+        this.renderSeries = this.renderSeries.bind(this);
+    }
+
+    // componentDidMount() {
+    //     this.setState({renderedOnce: true})
+    // }
+    //
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log(nextProps)
+        console.log(this.props)
+        if (this.props === nextProps) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     // Method for grabbing id and hero name when hero image btn is selected, having an issue with selcted property being undefined
@@ -66,10 +86,77 @@ series;
     //
     // }
 
+    renderSeries() {
+        for (let [k, v] of this.myMap) {
+            this.temp = v
+            console.log(k, v);
+            console.log(Math.floor(Math.random() * (this.temp.length)));
 
-
+            this.issue_cover_id = this.temp[Math.floor(Math.random() * (this.temp.length))]
+            this.image = require('./issue_covers/' + this.issue_cover_id + '.jpg');
+            return (
+                <div>
+                    <button>
+                        <img className="Hero-Images" src={this.image}/>
+                    </button>
+                </div>
+            )
+        }
+    }
 
     render() {
+
+        {
+                this.props.List.map(s => {
+                    // console.log(s.series_id + " " + s.id + " " + this.myMap.has(s.series_id))
+                    if (this.myMap.has(s.series_id) == false) {
+                        this.temp.push(s.id)
+                        this.myMap.set(s.series_id, this.temp);
+                        this.temp = []
+                    } else {
+                        {
+                            this.myMap.get(s.series_id).map(s => this.temp.push(s))
+                        }
+                        this.temp.push(s.id)
+                        this.myMap.set(s.series_id, this.temp)
+                        this.temp = []
+                    }
+                })
+        }
+
+        // for (let [k, v] of this.myMap) {
+        //     this.temp = v
+        //     console.log(k, v);
+        //     console.log(Math.floor(Math.random() * (this.temp.length)));
+        //
+        //     this.issue_cover_id = this.temp[Math.floor(Math.random() * (this.temp.length))]
+        //     this.image = require('./issue_covers/' + this.issue_cover_id + '.jpg');
+        //     return (
+        //         <div>
+        //             <button>
+        //                 <img className="Hero-Images" src={this.image}/>
+        //             </button>
+        //         </div>
+        //     )
+        // }
+
+        // {this.myMap.values().map(s => {
+        //     console.log(s)
+        // })
+        // }
+
+
+        // this.myMap.set("dog", 3)
+
+        console.log(this.myMap);
+
+        // {
+        //     this.props.List.map(s => {
+        //         console.log(s)
+        //     })
+        // }
+
+
         // console.log(this.state.List);
 
         // const imagesElements = this.state.list.map( e => {
@@ -128,17 +215,29 @@ series;
             <div>
                 {/*<h2 id="Quick-Fix">Series Covers</h2>*/}
                 <Slider className="Result-Slider" {...settings}>
-                    {this.props.List.map(s => {
-                        this.issue_cover_id = s.id;
-                        this.image = require('./issue_covers/' + this.issue_cover_id + '.jpg');
-                        return (
-                        <div key={s.id}>
-                            <button>
-                        <img className="Hero-Images" src={this.image}/>
-                            </button>
-                        </div>
-                        )
-                    })}
+                    {this.renderSeries()}
+                    {/*{this.myMap.values().map(s => {*/}
+                    {/*    this.issue_cover_id = s[0];*/}
+                    {/*    this.image = require('./issue_covers/' + this.issue_cover_id + '.jpg');*/}
+                    {/*    return (*/}
+                    {/*        <div key={s.id}>*/}
+                    {/*            <button>*/}
+                    {/*                <img className="Hero-Images" src={this.image}/>*/}
+                    {/*            </button>*/}
+                    {/*        </div>*/}
+                    {/*    )*/}
+                    {/*})}*/}
+                    {/*{this.props.List.map(s => {*/}
+                    {/*    this.issue_cover_id = s.id;*/}
+                    {/*    this.image = require('./issue_covers/' + this.issue_cover_id + '.jpg');*/}
+                    {/*    return (*/}
+                    {/*    <div key={s.id}>*/}
+                    {/*        <button>*/}
+                    {/*    <img className="Hero-Images" src={this.image}/>*/}
+                    {/*        </button>*/}
+                    {/*    </div>*/}
+                    {/*    )*/}
+                    {/*})}*/}
                 </Slider>
             </div>
         );
